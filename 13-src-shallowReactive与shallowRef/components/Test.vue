e8b659530848ee684e9d07244f93e385f520b041
Yoga2015 <yoga3333@126.com>
2023-05-14 16:47:57 +0800

    shallowReactive 与 shallowRef

---
+++ 13-src-shallowReactive与shallowRef/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <h3>当前的x的值是: {{ x }}</h3>
+  <button @click="x++">点我x+1</button>
+</template>
+
+<script>
+import { shallowRef } from 'vue'
+export default {
+  name: 'MyTest',
+  setup() {
+
+    // let x = ref(0)   基本数据类型
+    let x = shallowRef(0)
+
+    console.log(x);
+
+    return {
+      x
+    }
+  },
+}
+</script>
+
