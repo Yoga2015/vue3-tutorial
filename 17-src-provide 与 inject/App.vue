7e35d3031527f93b8c638cc0050cf53fdd48588a
Yoga2015 <yoga3333@126.com>
2023-05-15 19:57:30 +0800

    provide 与 inject

--- 17-src-provide 与 inject/App.vue
+++ 17-src-provide 与 inject/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="App">
     <h3>我是App组件 ( 祖 ) , {{ name }}--{{ price }}</h3>
+    <Child></Child>
   </div>
 </template>
 
 <script>
-
-import { reactive, toRefs } from 'vue'
+import Child from './components/Child.vue'
+import { provide, reactive, toRefs } from 'vue'
 export default {
   name: 'myApp',
+  components: { Child },
   setup() {
     let car = reactive({ name: '比亚迪', price: '20k' })
+    provide('car', car)  // 给自己的后代组件 传递 数据
     return { ...toRefs(car) }
   }
 }
 </script>
+
+<style>
+#App {
+  background-color: pink;
+  padding: 10px;
+}
+</style>
---
+++ 17-src-provide 与 inject/components/GrandSon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="GrandSon">
+    <h3>我是GrandSon组件 ( 孙 )</h3>
+    <!-- <h3>接收 祖宗 传递的数据：{{ carMsg }}</h3> -->
+    <h3>接收祖宗传递过来的数据：
+        {{ carMsg.name }}--{{ carMsg.price }}
+    </h3>
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue';
+export default {
+  name: 'myGrandSon',
+  setup() {
+    let carMsg = inject('car')  //接收祖宗传递的数据
+    console.log(carMsg);
+    return { carMsg }
+  }
+}
+</script>
+
+<style>
+#GrandSon {
+  background-color: orange;
+  padding: 10px;
+}
+</style>
